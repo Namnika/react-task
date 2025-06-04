098038794af9de02569643abbb5659256715a665
Namnika <janbandhunamnika@gmail.com>
2025-06-04 12:39:23 +0530

    add path to components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter >
+        <Navbar />
+        <main>
+          <section className="wrapper">
+            <Routes>
+              <Route path="/" element={<>
+                <Home />
+                <Project />
+              </>} />
+
+            </Routes>
+          </section>
+        </main>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
